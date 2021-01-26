2ff8b1929f92f990d210348835dcc4ab892e08d6
Somit <somitmeshram3256@gmail.com>
2021-01-26 14:38:16 +0530

    Filter added

--- js/app.js
+++ js/app.js
@@ -14,12 +14,15 @@ function loadEventListeners() {
   form.addEventListener('submit', addTask);
 
   //Remove task Event
-  taskList.addEventListener('click',removeTask);
+  taskList.addEventListener('click', removeTask);
+
+  //filter event
+  filter.addEventListener('keyup', filterTask);
 }
 
 // Add Task
 function addTask(e) {
-  if(taskInput.value === '') {
+  if (taskInput.value === '') {
     alert('Add a task');
     return;
   }
@@ -44,9 +47,23 @@ function addTask(e) {
   e.preventDefault();
 }
 
-function removeTask(e){
-    if(e.target.parentElement.classList.contains('delete-item')){
-        //console.log(e.target)
-        e.target.parentElement.parentElement.remove();// li->a->i
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    //console.log(e.target)
+    e.target.parentElement.parentElement.remove(); // li->a->i
+  }
+}
+
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
     }
+  });
+  // console.log(text)
+
 }
